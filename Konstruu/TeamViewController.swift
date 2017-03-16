a542f256359d4bab500c1857ac06ae4cd514fbaa
David Xue <dxue@stanford.edu>
2017-03-16 11:46:20 -0700

    Update the team view controller.

--- Konstruu/TeamViewController.swift
+++ Konstruu/TeamViewController.swift
@@ -10,7 +10,10 @@ import UIKit
 import DataStructures
 
 class TeamViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UICollectionViewDelegate, UICollectionViewDataSource {
-    
+  
+    let sectionInsets = UIEdgeInsets(top: 10.0, left: 20.0, bottom: 10.0, right: 20.0)
+//    let itemsPerRow: CGFloat = 4
+  
     // MARK: - Model
     
     var team: DataStructures.Team? {
@@ -33,10 +36,42 @@ class TeamViewController: UIViewController, UITableViewDelegate, UITableViewData
         usersCollectionView.register(UINib(nibName: "UserCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "user")
         progressPostsTableView.register(UINib(nibName: "ProgressPostTableViewCell", bundle: nil), forCellReuseIdentifier: "progressPost")
         updateUI()
+      
+        addSubviews()
+        addConstraints()
+    }
+  
+    func addSubviews() {
+      view.addSubview(backgroundView)
+      view.sendSubview(toBack: backgroundView)
     }
     
+    func addConstraints() {
+      //backgroundView
+      
+      //top
+      view.addConstraint(NSLayoutConstraint(item:backgroundView, attribute:.top, relatedBy:.equal, toItem: view, attribute:.top, multiplier: 1, constant: 0))
+      //bottom
+      view.addConstraint(NSLayoutConstraint(item:backgroundView, attribute:.bottom, relatedBy:.equal, toItem: view, attribute:.bottom, multiplier: 1, constant: 0))
+      //left
+      view.addConstraint(NSLayoutConstraint(item:backgroundView, attribute:.left, relatedBy:.equal, toItem: view, attribute:.left, multiplier: 1, constant: 0))
+      //right
+      view.addConstraint(NSLayoutConstraint(item:backgroundView, attribute:.right, relatedBy:.equal, toItem: view, attribute:.right, multiplier: 1, constant: 0))
+      
+      view.layoutIfNeeded()
+    }
+  
     // MARK: - UI
-    
+  
+    private lazy var backgroundView: UIImageView = { [unowned self] in
+      let backgroundView = UIImageView()
+      backgroundView.image = UIImage(named: "profileGradient.png")
+      backgroundView.contentMode = UIViewContentMode.scaleAspectFill
+      
+      backgroundView.translatesAutoresizingMaskIntoConstraints = false
+      return backgroundView
+      }()
+  
     @IBOutlet weak var teamNameLabel: UILabel!
     
     @IBOutlet weak var challengeButton: UIButton!
@@ -71,7 +106,7 @@ class TeamViewController: UIViewController, UITableViewDelegate, UITableViewData
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = usersCollectionView.dequeueReusableCell(withReuseIdentifier: "user", for: indexPath)
         
@@ -122,3 +157,29 @@ class TeamViewController: UIViewController, UITableViewDelegate, UITableViewData
         self.navigationController?.pushViewController(challengeVC, animated: true)
     }
 }
+
+extension TeamViewController : UICollectionViewDelegateFlowLayout {
+  
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      sizeForItemAt indexPath: IndexPath) -> CGSize {
+//    let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+//    let availableWidth = view.frame.width - paddingSpace
+//    let widthPerItem = availableWidth / itemsPerRow
+    
+    return CGSize(width: 64.0, height: 64.0)
+  }
+  
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      insetForSectionAt section: Int) -> UIEdgeInsets {
+    return sectionInsets
+  }
+  
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+    return sectionInsets.left
+  }
+}
+
