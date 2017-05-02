ed123ba2037c23cee0ada8f7a367c9de4a16b452
David Xue <dxue@stanford.edu>
2017-05-02 09:38:40 -0700

    Connected pushed views

--- Konstruu/ChallengeCategoriesViewController.swift
+++ Konstruu/ChallengeCategoriesViewController.swift
@@ -10,24 +10,28 @@ import UIKit
 
 class ChallengeCategoriesViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+  // MARK: - UI
+  
+  @IBOutlet weak var addChallengeButton: UIButton!
+  
+  // MARK: - Actions
+  
+  @IBAction func addChallenge(_ sender: Any) {
+    let createChallengeVC = CreateChallengeViewController()
+    self.navigationController?.pushViewController(createChallengeVC, animated: true)
+  }
+  
+  // MARK: - View Lifecycle
+  
+  override func viewDidLoad() {
+      super.viewDidLoad()
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+  }
+  
+  
 
 }
