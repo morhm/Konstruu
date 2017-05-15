501ac6e2a236e00a71ed1500fc220d4db9a0d114
David Xue <dxue@stanford.edu>
2017-05-15 11:23:30 -0700

    Profile view teams is linked up to backend

--- Konstruu/ProfileProjectTableViewCell.swift
+++ Konstruu/ProfileProjectTableViewCell.swift
@@ -20,16 +20,16 @@ private extension CGFloat {
   static let imageViewHeightConstraint: CGFloat      = 80.0
   static let imageViewWidthConstraint: CGFloat       = 80.0
   
-  static let nameLabelTopConstraint:CGFloat           = 8.0
-  static let nameLabelLeftConstraint:CGFloat          = 20.0
-  static let nameLabelRightConstraint:CGFloat         = 8.0
-  static let nameLabelHeightConstraint:CGFloat        = 25.0
+  static let tableViewTopConstraint:CGFloat           = 8.0
+  static let tableViewBottomConstraint:CGFloat        = 8.0
+  static let tableViewLeftConstraint:CGFloat          = 8.0
+  static let tableViewRightConstraint:CGFloat         = 8.0
   
   static let descriptionLabelTopConstraint:CGFloat    = 0.0
   static let descriptionLabelHeightConstraint:CGFloat = 60.0
 }
 
-class ProfileProjectTableViewCell: UITableViewCell {
+class ProfileProjectTableViewCell: UITableViewCell, UITableViewDelegate, UITableViewDataSource {
   
   // MARK: - Data
   
@@ -47,6 +47,8 @@ class ProfileProjectTableViewCell: UITableViewCell {
       secondProjectNameLabel.text = teams?[1].name
     }
   }
+  
+  var navController: UINavigationController?
   
   private var updated = false
   
@@ -70,6 +72,18 @@ class ProfileProjectTableViewCell: UITableViewCell {
     cardView.translatesAutoresizingMaskIntoConstraints = false
     return cardView
     }()
+  
+  private lazy var teamsTableView: UITableView = { [unowned self] in
+    let teamsTableView = UITableView()
+    teamsTableView.backgroundColor = UIColor.clear
+    
+    teamsTableView.delegate = self
+    teamsTableView.dataSource = self
+  
+    teamsTableView.translatesAutoresizingMaskIntoConstraints = false
+    return teamsTableView
+  }()
+
   
   private lazy var firstProjectNameLabel : UILabel = { [unowned self] in
     let nameLabel = UILabel()
@@ -131,11 +145,13 @@ class ProfileProjectTableViewCell: UITableViewCell {
     
     contentView.addSubview(titleLabel)
     
-    contentView.addSubview(firstProjectNameLabel)
-    contentView.addSubview(firstProjectDescriptionLabel)
-
-    contentView.addSubview(secondProjectNameLabel)
-    contentView.addSubview(secondProjectDescriptionLabel)
+    contentView.addSubview(teamsTableView)
+    
+//    contentView.addSubview(firstProjectNameLabel)
+//    contentView.addSubview(firstProjectDescriptionLabel)
+//
+//    contentView.addSubview(secondProjectNameLabel)
+//    contentView.addSubview(secondProjectDescriptionLabel)
     
     updateConstraints()
   }
@@ -175,50 +191,95 @@ class ProfileProjectTableViewCell: UITableViewCell {
     contentView.addConstraint(NSLayoutConstraint(item:titleLabel, attribute:.height, relatedBy:.equal, toItem: nil, attribute:.notAnAttribute, multiplier: 1, constant: .titleLabelHeightConstraint))
     
     
-    //firstProjectNameLabel
+    //tableView
     
-    //left
-    contentView.addConstraint(NSLayoutConstraint(item:firstProjectNameLabel, attribute:.left, relatedBy:.equal, toItem: contentView, attribute:.left, multiplier: 1, constant: .nameLabelLeftConstraint))
-    //right
-    contentView.addConstraint(NSLayoutConstraint(item:firstProjectNameLabel, attribute:.right, relatedBy:.equal, toItem: contentView, attribute:.right, multiplier: 1, constant: -.nameLabelRightConstraint))
     //top
-    contentView.addConstraint(NSLayoutConstraint(item:firstProjectNameLabel, attribute:.top, relatedBy:.equal, toItem: titleLabel, attribute:.bottom, multiplier: 1, constant: .nameLabelTopConstraint))
-    //height
-    contentView.addConstraint(NSLayoutConstraint(item:firstProjectNameLabel, attribute:.height, relatedBy:.equal, toItem: nil, attribute:.notAnAttribute, multiplier: 1, constant: .nameLabelHeightConstraint))
-    
-    //firstProjectDescriptionLabel
-    
+    contentView.addConstraint(NSLayoutConstraint(item:teamsTableView, attribute:.top, relatedBy:.equal, toItem: titleLabel, attribute:.bottom, multiplier: 1, constant: .tableViewTopConstraint))
     //left
-    contentView.addConstraint(NSLayoutConstraint(item:firstProjectDescriptionLabel, attribute:.left, relatedBy:.equal, toItem: firstProjectNameLabel, attribute:.left, multiplier: 1, constant: 0))
+    contentView.addConstraint(NSLayoutConstraint(item:teamsTableView, attribute:.left, relatedBy:.equal, toItem: contentView, attribute:.left, multiplier: 1, constant: .tableViewLeftConstraint))
     //right
-    contentView.addConstraint(NSLayoutConstraint(item:firstProjectDescriptionLabel, attribute:.right, relatedBy:.equal, toItem: firstProjectNameLabel, attribute:.right, multiplier: 1, constant: 0))
-    //top
-    contentView.addConstraint(NSLayoutConstraint(item:firstProjectDescriptionLabel, attribute:.top, relatedBy:.equal, toItem: firstProjectNameLabel, attribute:.bottom, multiplier: 1, constant: .descriptionLabelTopConstraint))
-    //height
-    contentView.addConstraint(NSLayoutConstraint(item:firstProjectDescriptionLabel, attribute:.height, relatedBy:.equal, toItem: nil, attribute:.notAnAttribute, multiplier: 1, constant: .descriptionLabelHeightConstraint))
+    contentView.addConstraint(NSLayoutConstraint(item:teamsTableView, attribute:.right, relatedBy:.equal, toItem: contentView, attribute:.right, multiplier: 1, constant: -.tableViewRightConstraint))
+    //bottom
+    contentView.addConstraint(NSLayoutConstraint(item:teamsTableView, attribute:.bottom, relatedBy:.equal, toItem: contentView, attribute:.bottom, multiplier: 1, constant: -.tableViewBottomConstraint))
     
-    //secondProjectNameLabel
-    
-    //left
-    contentView.addConstraint(NSLayoutConstraint(item:secondProjectNameLabel, attribute:.left, relatedBy:.equal, toItem: contentView, attribute:.left, multiplier: 1, constant: .nameLabelLeftConstraint))
-    //right
-    contentView.addConstraint(NSLayoutConstraint(item:secondProjectNameLabel, attribute:.right, relatedBy:.equal, toItem: contentView, attribute:.right, multiplier: 1, constant: -.nameLabelRightConstraint))
-    //top
-    contentView.addConstraint(NSLayoutConstraint(item:secondProjectNameLabel, attribute:.top, relatedBy:.equal, toItem: firstProjectDescriptionLabel, attribute:.bottom, multiplier: 1, constant: .nameLabelTopConstraint))
-    //height
-    contentView.addConstraint(NSLayoutConstraint(item:secondProjectNameLabel, attribute:.height, relatedBy:.equal, toItem: nil, attribute:.notAnAttribute, multiplier: 1, constant: .nameLabelHeightConstraint))
-    
-    //secondProjectDescriptionLabel
-    
-    //left
-    contentView.addConstraint(NSLayoutConstraint(item:secondProjectDescriptionLabel, attribute:.left, relatedBy:.equal, toItem: secondProjectNameLabel, attribute:.left, multiplier: 1, constant: 0))
-    //right
-    contentView.addConstraint(NSLayoutConstraint(item:secondProjectDescriptionLabel, attribute:.right, relatedBy:.equal, toItem: secondProjectNameLabel, attribute:.right, multiplier: 1, constant: 0))
-    //top
-    contentView.addConstraint(NSLayoutConstraint(item:secondProjectDescriptionLabel, attribute:.top, relatedBy:.equal, toItem: secondProjectNameLabel, attribute:.bottom, multiplier: 1, constant: .descriptionLabelTopConstraint))
-    //height
-    contentView.addConstraint(NSLayoutConstraint(item:secondProjectDescriptionLabel, attribute:.height, relatedBy:.equal, toItem: nil, attribute:.notAnAttribute, multiplier: 1, constant: .descriptionLabelHeightConstraint))
-    
+//    //firstProjectNameLabel
+//    
+//    //left
+//    contentView.addConstraint(NSLayoutConstraint(item:firstProjectNameLabel, attribute:.left, relatedBy:.equal, toItem: contentView, attribute:.left, multiplier: 1, constant: .nameLabelLeftConstraint))
+//    //right
+//    contentView.addConstraint(NSLayoutConstraint(item:firstProjectNameLabel, attribute:.right, relatedBy:.equal, toItem: contentView, attribute:.right, multiplier: 1, constant: -.nameLabelRightConstraint))
+//    //top
+//    contentView.addConstraint(NSLayoutConstraint(item:firstProjectNameLabel, attribute:.top, relatedBy:.equal, toItem: titleLabel, attribute:.bottom, multiplier: 1, constant: .nameLabelTopConstraint))
+//    //height
+//    contentView.addConstraint(NSLayoutConstraint(item:firstProjectNameLabel, attribute:.height, relatedBy:.equal, toItem: nil, attribute:.notAnAttribute, multiplier: 1, constant: .nameLabelHeightConstraint))
+//    
+//    //firstProjectDescriptionLabel
+//    
+//    //left
+//    contentView.addConstraint(NSLayoutConstraint(item:firstProjectDescriptionLabel, attribute:.left, relatedBy:.equal, toItem: firstProjectNameLabel, attribute:.left, multiplier: 1, constant: 0))
+//    //right
+//    contentView.addConstraint(NSLayoutConstraint(item:firstProjectDescriptionLabel, attribute:.right, relatedBy:.equal, toItem: firstProjectNameLabel, attribute:.right, multiplier: 1, constant: 0))
+//    //top
+//    contentView.addConstraint(NSLayoutConstraint(item:firstProjectDescriptionLabel, attribute:.top, relatedBy:.equal, toItem: firstProjectNameLabel, attribute:.bottom, multiplier: 1, constant: .descriptionLabelTopConstraint))
+//    //height
+//    contentView.addConstraint(NSLayoutConstraint(item:firstProjectDescriptionLabel, attribute:.height, relatedBy:.equal, toItem: nil, attribute:.notAnAttribute, multiplier: 1, constant: .descriptionLabelHeightConstraint))
+//    
+//    //secondProjectNameLabel
+//    
+//    //left
+//    contentView.addConstraint(NSLayoutConstraint(item:secondProjectNameLabel, attribute:.left, relatedBy:.equal, toItem: contentView, attribute:.left, multiplier: 1, constant: .nameLabelLeftConstraint))
+//    //right
+//    contentView.addConstraint(NSLayoutConstraint(item:secondProjectNameLabel, attribute:.right, relatedBy:.equal, toItem: contentView, attribute:.right, multiplier: 1, constant: -.nameLabelRightConstraint))
+//    //top
+//    contentView.addConstraint(NSLayoutConstraint(item:secondProjectNameLabel, attribute:.top, relatedBy:.equal, toItem: firstProjectDescriptionLabel, attribute:.bottom, multiplier: 1, constant: .nameLabelTopConstraint))
+//    //height
+//    contentView.addConstraint(NSLayoutConstraint(item:secondProjectNameLabel, attribute:.height, relatedBy:.equal, toItem: nil, attribute:.notAnAttribute, multiplier: 1, constant: .nameLabelHeightConstraint))
+//    
+//    //secondProjectDescriptionLabel
+//    
+//    //left
+//    contentView.addConstraint(NSLayoutConstraint(item:secondProjectDescriptionLabel, attribute:.left, relatedBy:.equal, toItem: secondProjectNameLabel, attribute:.left, multiplier: 1, constant: 0))
+//    //right
+//    contentView.addConstraint(NSLayoutConstraint(item:secondProjectDescriptionLabel, attribute:.right, relatedBy:.equal, toItem: secondProjectNameLabel, attribute:.right, multiplier: 1, constant: 0))
+//    //top
+//    contentView.addConstraint(NSLayoutConstraint(item:secondProjectDescriptionLabel, attribute:.top, relatedBy:.equal, toItem: secondProjectNameLabel, attribute:.bottom, multiplier: 1, constant: .descriptionLabelTopConstraint))
+//    //height
+//    contentView.addConstraint(NSLayoutConstraint(item:secondProjectDescriptionLabel, attribute:.height, relatedBy:.equal, toItem: nil, attribute:.notAnAttribute, multiplier: 1, constant: .descriptionLabelHeightConstraint))
+//    
     super.updateConstraints()
+  }
+  
+  // MARK: - Table view data source
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return user?.teamKeys.count ?? 0
+    
+  }
+  
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "team", for: indexPath)
+    if let teamCell = (cell as? TeamTableViewCell),
+      let teamKey = user?.teamKeys[indexPath.row] {
+      API.getTeamWithKey(teamKey, completed: { team in
+        teamCell.team = team
+      })
+      return teamCell
+    }
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let teamVC = TeamViewController(nibName: "TeamViewController", bundle: nil)
+    if let teamKey = user?.teamKeys[indexPath.row] {
+      API.getTeamWithKey(teamKey, completed: { team in
+        teamVC.team = team
+        teamVC.navigationItem.rightBarButtonItem = KonstruuTabBarController.messagingButtonItem
+        self.navController?.pushViewController(teamVC, animated: true)
+      })
+    }
   }
 }
