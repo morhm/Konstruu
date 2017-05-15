1ca9887db2f226447ae22706c76b19582b106676
David Xue <dxue@stanford.edu>
2017-05-15 10:53:13 -0700

    Finished editing for specialities view.

--- Konstruu/ProfileProjectTableViewCell.swift
+++ Konstruu/ProfileProjectTableViewCell.swift
@@ -12,7 +12,7 @@ private extension CGFloat {
   static let cardViewTopConstraint:CGFloat           = 10.0
   
   static let titleLabelTopConstraint:CGFloat         = 10.0
-  static let titleLabelHeightConstraint:CGFloat      = 20.0
+  static let titleLabelHeightConstraint:CGFloat      = 30.0
   static let titleLabelLeftConstraint:CGFloat        = 15.0
   
   static let imageViewLeftConstraint: CGFloat        = 10.0
@@ -20,28 +20,31 @@ private extension CGFloat {
   static let imageViewHeightConstraint: CGFloat      = 80.0
   static let imageViewWidthConstraint: CGFloat       = 80.0
   
+  static let nameLabelTopConstraint:CGFloat           = 8.0
   static let nameLabelLeftConstraint:CGFloat          = 20.0
   static let nameLabelRightConstraint:CGFloat         = 8.0
   static let nameLabelHeightConstraint:CGFloat        = 25.0
   
   static let descriptionLabelTopConstraint:CGFloat    = 0.0
+  static let descriptionLabelHeightConstraint:CGFloat = 60.0
 }
 
 class ProfileProjectTableViewCell: UITableViewCell {
   
   // MARK: - Data
   
-  var projectNameText:String = "Frog and Code" {
+  var user: User? {
     didSet {
-      firstProjectNameLabel.text = projectNameText
-      secondProjectNameLabel.text = projectNameText
+      user?.getTeams(completed: { [weak self] teams in
+        self?.teams = teams
+      })
     }
   }
   
-  var projectDescriptionText:String = "We are building an app to bring students together to work on challenges within their community" {
+  var teams: [Team]? {
     didSet {
-      firstProjectDescriptionLabel.text = projectDescriptionText
-      secondProjectDescriptionLabel.text = projectDescriptionText
+      firstProjectNameLabel.text = teams?[0].name
+      secondProjectNameLabel.text = teams?[1].name
     }
   }
   
@@ -52,7 +55,7 @@ class ProfileProjectTableViewCell: UITableViewCell {
   private lazy var titleLabel : UILabel = { [unowned self] in
     let titleLabel = UILabel()
     titleLabel.textColor = UIColor.black
-    titleLabel.text  = "Projects"
+    titleLabel.text  = "Teams"
     titleLabel.textAlignment = .left
     titleLabel.font = UIFont.konstruuLightFontWithSize(18.0)
     
@@ -68,28 +71,10 @@ class ProfileProjectTableViewCell: UITableViewCell {
     return cardView
     }()
   
-  private lazy var firstProjectImageView: UIImageView = { [unowned self] in
-    let projectImageView = UIImageView()
-    projectImageView.image = UIImage(named: "high_school.png")
-    projectImageView.contentMode = .scaleAspectFill
-    
-    projectImageView.translatesAutoresizingMaskIntoConstraints = false
-    return projectImageView
-    }()
-  
-  private lazy var secondProjectImageView: UIImageView = { [unowned self] in
-    let projectImageView = UIImageView()
-    projectImageView.image = UIImage(named: "high_school.png")
-    projectImageView.contentMode = .scaleAspectFill
-    
-    projectImageView.translatesAutoresizingMaskIntoConstraints = false
-    return projectImageView
-    }()
-  
   private lazy var firstProjectNameLabel : UILabel = { [unowned self] in
     let nameLabel = UILabel()
     nameLabel.textColor = UIColor.black
-    nameLabel.text  = self.projectNameText
+    nameLabel.text  = self.teams?[0].name
     nameLabel.textAlignment = .left
     nameLabel.font = UIFont.konstruuLightFontWithSize(18.0)
     
@@ -100,7 +85,7 @@ class ProfileProjectTableViewCell: UITableViewCell {
   private lazy var secondProjectNameLabel : UILabel = { [unowned self] in
     let nameLabel = UILabel()
     nameLabel.textColor = UIColor.black
-    nameLabel.text  = self.projectNameText
+    nameLabel.text  = self.teams?[1].name
     nameLabel.textAlignment = .left
     nameLabel.font = UIFont.konstruuLightFontWithSize(18.0)
     
@@ -111,7 +96,7 @@ class ProfileProjectTableViewCell: UITableViewCell {
   private lazy var firstProjectDescriptionLabel : UILabel = { [unowned self] in
     let descriptionLabel = UILabel()
     descriptionLabel.textColor = UIColor.black
-    descriptionLabel.text  = self.projectDescriptionText
+    descriptionLabel.text  = "Test"
     descriptionLabel.textAlignment = .left
     descriptionLabel.numberOfLines = 0
     descriptionLabel.font = UIFont.konstruuLightFontWithSize(13.0)
@@ -123,7 +108,7 @@ class ProfileProjectTableViewCell: UITableViewCell {
   private lazy var secondProjectDescriptionLabel : UILabel = { [unowned self] in
     let descriptionLabel = UILabel()
     descriptionLabel.textColor = UIColor.black
-    descriptionLabel.text  = self.projectDescriptionText
+    descriptionLabel.text  = "Test"
     descriptionLabel.textAlignment = .left
     descriptionLabel.numberOfLines = 0
     descriptionLabel.font = UIFont.konstruuLightFontWithSize(13.0)
@@ -146,11 +131,9 @@ class ProfileProjectTableViewCell: UITableViewCell {
     
     contentView.addSubview(titleLabel)
     
-    contentView.addSubview(firstProjectImageView)
     contentView.addSubview(firstProjectNameLabel)
     contentView.addSubview(firstProjectDescriptionLabel)
 
-    contentView.addSubview(secondProjectImageView)
     contentView.addSubview(secondProjectNameLabel)
     contentView.addSubview(secondProjectDescriptionLabel)
     
@@ -179,7 +162,7 @@ class ProfileProjectTableViewCell: UITableViewCell {
     //bottom
     contentView.addConstraint(NSLayoutConstraint(item:cardView, attribute:.bottom, relatedBy:.equal, toItem: contentView, attribute:.bottom, multiplier: 1, constant: 0))
     
-    
+
     //titleLabel
     
     //top
@@ -191,26 +174,15 @@ class ProfileProjectTableViewCell: UITableViewCell {
     //height
     contentView.addConstraint(NSLayoutConstraint(item:titleLabel, attribute:.height, relatedBy:.equal, toItem: nil, attribute:.notAnAttribute, multiplier: 1, constant: .titleLabelHeightConstraint))
     
-    //firstProjectImageView
-    
-    //top
-    contentView.addConstraint(NSLayoutConstraint(item:firstProjectImageView, attribute:.top, relatedBy:.equal, toItem: titleLabel, attribute:.bottom, multiplier: 1, constant: .imageViewTopConstraint))
-    //left
-    contentView.addConstraint(NSLayoutConstraint(item:firstProjectImageView, attribute:.left, relatedBy:.equal, toItem: contentView, attribute:.left, multiplier: 1, constant: .imageViewLeftConstraint))
-    //width
-    contentView.addConstraint(NSLayoutConstraint(item:firstProjectImageView, attribute:.width, relatedBy:.equal, toItem: nil, attribute:.notAnAttribute, multiplier: 1, constant: .imageViewWidthConstraint))
-    //height
-    contentView.addConstraint(NSLayoutConstraint(item:firstProjectImageView, attribute:.height, relatedBy:.equal, toItem: nil, attribute:.notAnAttribute, multiplier: 1, constant: .imageViewHeightConstraint))
-    
     
     //firstProjectNameLabel
     
     //left
-    contentView.addConstraint(NSLayoutConstraint(item:firstProjectNameLabel, attribute:.left, relatedBy:.equal, toItem: firstProjectImageView, attribute:.right, multiplier: 1, constant: .nameLabelLeftConstraint))
+    contentView.addConstraint(NSLayoutConstraint(item:firstProjectNameLabel, attribute:.left, relatedBy:.equal, toItem: contentView, attribute:.left, multiplier: 1, constant: .nameLabelLeftConstraint))
     //right
     contentView.addConstraint(NSLayoutConstraint(item:firstProjectNameLabel, attribute:.right, relatedBy:.equal, toItem: contentView, attribute:.right, multiplier: 1, constant: -.nameLabelRightConstraint))
     //top
-    contentView.addConstraint(NSLayoutConstraint(item:firstProjectNameLabel, attribute:.top, relatedBy:.equal, toItem: firstProjectImageView, attribute:.top, multiplier: 1, constant: 0))
+    contentView.addConstraint(NSLayoutConstraint(item:firstProjectNameLabel, attribute:.top, relatedBy:.equal, toItem: titleLabel, attribute:.bottom, multiplier: 1, constant: .nameLabelTopConstraint))
     //height
     contentView.addConstraint(NSLayoutConstraint(item:firstProjectNameLabel, attribute:.height, relatedBy:.equal, toItem: nil, attribute:.notAnAttribute, multiplier: 1, constant: .nameLabelHeightConstraint))
     
@@ -222,29 +194,17 @@ class ProfileProjectTableViewCell: UITableViewCell {
     contentView.addConstraint(NSLayoutConstraint(item:firstProjectDescriptionLabel, attribute:.right, relatedBy:.equal, toItem: firstProjectNameLabel, attribute:.right, multiplier: 1, constant: 0))
     //top
     contentView.addConstraint(NSLayoutConstraint(item:firstProjectDescriptionLabel, attribute:.top, relatedBy:.equal, toItem: firstProjectNameLabel, attribute:.bottom, multiplier: 1, constant: .descriptionLabelTopConstraint))
-    //bottom
-    contentView.addConstraint(NSLayoutConstraint(item:firstProjectDescriptionLabel, attribute:.bottom, relatedBy:.equal, toItem: firstProjectImageView, attribute:.bottom, multiplier: 1, constant: 0))
-    
-    //secondProjectImageView
-    
-    //left
-    contentView.addConstraint(NSLayoutConstraint(item:secondProjectImageView, attribute:.left, relatedBy:.equal, toItem: contentView, attribute:.left, multiplier: 1, constant: .imageViewLeftConstraint))
-    //top
-    contentView.addConstraint(NSLayoutConstraint(item:secondProjectImageView, attribute:.top, relatedBy:.equal, toItem: firstProjectImageView, attribute:.bottom, multiplier: 1, constant: .imageViewTopConstraint))
-    //width
-    contentView.addConstraint(NSLayoutConstraint(item:secondProjectImageView, attribute:.width, relatedBy:.equal, toItem: nil, attribute:.notAnAttribute, multiplier: 1, constant: .imageViewWidthConstraint))
     //height
-    contentView.addConstraint(NSLayoutConstraint(item:secondProjectImageView, attribute:.height, relatedBy:.equal, toItem: nil, attribute:.notAnAttribute, multiplier: 1, constant: .imageViewHeightConstraint))
-    
+    contentView.addConstraint(NSLayoutConstraint(item:firstProjectDescriptionLabel, attribute:.height, relatedBy:.equal, toItem: nil, attribute:.notAnAttribute, multiplier: 1, constant: .descriptionLabelHeightConstraint))
     
     //secondProjectNameLabel
     
     //left
-    contentView.addConstraint(NSLayoutConstraint(item:secondProjectNameLabel, attribute:.left, relatedBy:.equal, toItem: secondProjectImageView, attribute:.right, multiplier: 1, constant: .nameLabelLeftConstraint))
+    contentView.addConstraint(NSLayoutConstraint(item:secondProjectNameLabel, attribute:.left, relatedBy:.equal, toItem: contentView, attribute:.left, multiplier: 1, constant: .nameLabelLeftConstraint))
     //right
     contentView.addConstraint(NSLayoutConstraint(item:secondProjectNameLabel, attribute:.right, relatedBy:.equal, toItem: contentView, attribute:.right, multiplier: 1, constant: -.nameLabelRightConstraint))
     //top
-    contentView.addConstraint(NSLayoutConstraint(item:secondProjectNameLabel, attribute:.top, relatedBy:.equal, toItem: secondProjectImageView, attribute:.top, multiplier: 1, constant: 0))
+    contentView.addConstraint(NSLayoutConstraint(item:secondProjectNameLabel, attribute:.top, relatedBy:.equal, toItem: firstProjectDescriptionLabel, attribute:.bottom, multiplier: 1, constant: .nameLabelTopConstraint))
     //height
     contentView.addConstraint(NSLayoutConstraint(item:secondProjectNameLabel, attribute:.height, relatedBy:.equal, toItem: nil, attribute:.notAnAttribute, multiplier: 1, constant: .nameLabelHeightConstraint))
     
@@ -256,8 +216,8 @@ class ProfileProjectTableViewCell: UITableViewCell {
     contentView.addConstraint(NSLayoutConstraint(item:secondProjectDescriptionLabel, attribute:.right, relatedBy:.equal, toItem: secondProjectNameLabel, attribute:.right, multiplier: 1, constant: 0))
     //top
     contentView.addConstraint(NSLayoutConstraint(item:secondProjectDescriptionLabel, attribute:.top, relatedBy:.equal, toItem: secondProjectNameLabel, attribute:.bottom, multiplier: 1, constant: .descriptionLabelTopConstraint))
-    //bottom
-    contentView.addConstraint(NSLayoutConstraint(item:secondProjectDescriptionLabel, attribute:.bottom, relatedBy:.equal, toItem: secondProjectImageView, attribute:.bottom, multiplier: 1, constant: 0))
+    //height
+    contentView.addConstraint(NSLayoutConstraint(item:secondProjectDescriptionLabel, attribute:.height, relatedBy:.equal, toItem: nil, attribute:.notAnAttribute, multiplier: 1, constant: .descriptionLabelHeightConstraint))
     
     super.updateConstraints()
   }
